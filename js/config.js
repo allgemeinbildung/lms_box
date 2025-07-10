28906071ac15b5284140b1fc4dd59646d521ae89
Pietro Rossi <LP4PRossi@gibwintra.ch>
2025-07-10 11:01:09 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Rename this file to "config.js" and paste your Google Apps Script Web App URL here.
 // DO NOT commit the "config.js" file to your repository if it's public.
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxtZyvs_2pJ3qf68lYIKQdL5Aj8jsxSPQe1BR1Kt23f7bRKkIvwwNsELq9jcKG0rGwMOA/exec';
+export const SCRIPT_URL = 'https://europe-west6-keen-surfer-465508-f2.cloudfunctions.net/assignmentHandler';
